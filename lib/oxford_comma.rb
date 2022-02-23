9587f22c0c3ff08f55c3ca3960c7c0321987c413
David <Davidmaxsands@gmail.com>
2022-02-23 15:29:02 -0700

    commit tree f9e011013787413dc54345dbaffa432753e48829

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if array.length > 2 
+        array[0..-2].join(", ") + ", and " + array[-1]
+    elsif array.length == 2
+        array.join(" and ")
+        else
+            array[0]
+    end
 end
+
+puts oxford_comma(["Dogs", "Cats", "Fish", "Friends"])
